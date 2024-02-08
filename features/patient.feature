0c32b4e5608274113d9af11d6bc4f7e1b24678b0
Kelkar <AKELKAR1@jaguarlandrover.com>
2024-02-08 06:26:42 +0000

    added patient feature

--- features/patient.feature
+++ features/patient.feature
@@ -1,21 +1,26 @@
+@patient
 Feature: Patient
-  In order to maintain patients record
-  As a admin
-  I want to add edit delete patients and details
+  In order to maintain the patients record
+  as a user
+  I want to add, edit, delete the patient details
 
   @addpatient
-  Scenario: AddValidPatient
+  Scenario Outline: Add Valid Patient Record
     Given I have browser with OpenEMR application
-    When I enter username as "admin"
-    And I enter password as "pass"
+    When I enter username as "<username>"
+    And I enter password as "<password>"
     And I click on login
-    And I click on patient menu
-    And I click on new search menu
-    And I fill patient details form
-      | firstname | lastname | DOB |  Gender|
-      | john      |john123 | 12/1/2023 | Male |
+    And I click on Patient menu
+    And I click on New Search menu
+    And I fill the patient details form
+      | firstname | lastname   | dob   | gender   |
+      | <fname>   | <lastname> | <dob> | <gender> |
     And I click on create new patient
-    And I click on confirm create patient
-    And I handle alert box
-    And I close birthday popup if available
-    Then I should get added patient record
+    And I click on confirm create new patient
+    And I handle the alert box
+    And I close the birthday popup if available
+    Then I should get the added patient record as "<expected_patient_name>"
+    Examples:
+      | username | password | fname | lastname | dob        | gender | expected_patient_name                |
+      | admin    | pass     | john  | wick     | 2024-02-08 | Female | Medical Record Dashboard - john wick |
+      | admin    | pass     | kim   | saul     | 2024-02-08 | Male   | Medical Record Dashboard - kim saul  |
--- features/steps/patient_steps.py
+++ features/steps/patient_steps.py
@@ -1,44 +1,47 @@
-from assertpy import assert_that
 from behave import *
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 
-@when(u'I click on patient menu')
+@when(u'I click on Patient menu')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click on patient menu')
+    context.driver.find_element(By.XPATH, "//div[text()='Patient']").click()
 
 
-@when(u'I click on new search menu')
+@when(u'I click on New Search menu')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click on new search menu')
+    context.driver.find_element(By.XPATH, "//div[text()='New/Search']").click()
 
 
-@when(u'I fill patient details form')
+@when(u'I fill the patient details form')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I fill patient details form')
+    context.driver.switch_to.frame(context.driver.find_element(By.XPATH,"//iframe[@name='pat']"))
+    context.driver.find_element(By.ID, "form_fname").send_keys(context.table.rows[0]["firstname"])
+    context.driver.find_element(By.CSS_SELECTOR, "#form_lname").send_keys(context.table.rows[0]["lastname"])
+    # print(len(context.table.rows))
+    #enter dob
+    #select gender
 
 
 @when(u'I click on create new patient')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click on create new patient')
+    print("hi")
 
 
-@when(u'I click on confirm create patient')
+@when(u'I click on confirm create new patient')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I click on confirm create patient')
+    print("hi")
 
 
-@when(u'I handle alert box')
+@when(u'I handle the alert box')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I handle alert box')
+    print("hi")
 
 
-@when(u'I close birthday popup if available')
+@when(u'I close the birthday popup if available')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: When I close birthday popup if available')
+    print("hi")
 
 
-@then(u'I should get added patient record')
-def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should get added patient record')
+@then(u'I should get the added patient record as "{expected_patient_name}"')
+def step_impl(context,expected_patient_name):
+    print(expected_patient_name)
